4d76dc31b2cd59bafb49684042104a0c26229e01
Vlad@Aqirys <vlad.mocanu.alexandru@gmail.com>
2022-12-24 11:25:44 +0200

    Renamed templates

--- setup_new_day.py
+++ setup_new_day.py
@@ -5,6 +5,6 @@ dayFolder = path = os.path.join(yearFolder, 'd'+"{:02d}".format(int(input("Day:"
 
 os.mkdir(dayFolder)
 
-shutil.copyfile(os.path.join('templates', 'input.txt'), os.path.join(dayFolder, 'input.txt'))
-shutil.copyfile(os.path.join('templates', 'p1.py'), os.path.join(dayFolder, 'p1.py'))
-shutil.copyfile(os.path.join('templates', 'p2.py'), os.path.join(dayFolder, 'p2.py'))
+shutil.copyfile(os.path.join('templates', 'input_template.txt'), os.path.join(dayFolder, 'input.txt'))
+shutil.copyfile(os.path.join('templates', 'p1_template.py'), os.path.join(dayFolder, 'p1.py'))
+shutil.copyfile(os.path.join('templates', 'p2_template.py'), os.path.join(dayFolder, 'p2.py'))
