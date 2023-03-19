be3acacb5db8f62a8633a0303135d18084cf1363
Vlad (macbook) <vlad.mocanu.alexandru@gmail.com>
2023-03-20 00:32:07 +0200

    implemented bad sollution for 2015/13

--- y2015/d13/p1.py
+++ y2015/d13/p1.py
@@ -12,15 +12,47 @@ from utils.labelMakerUtils import *
 from utils.solutionRoot import *
  
 def getInputData(inputFile):
-    raw = getRawLines(inputFile)
+    raw = getTuples_text(inputFile, " would ", " happiness units by sitting next to ", ".")
     
-    processed=[entry for entry in raw]
+    processed=pd.DataFrame([entry[:-1]  for entry in raw], columns=["nodeA", "weight_dirty", "nodeB"])
+    processed["weight"] = processed["weight_dirty"].apply(lambda e : int(e.replace("gain ", "").replace("lose ", "")) * (-1 if "lose" in e else 1))
+    processed.drop("weight_dirty", axis=1, inplace=True)
 
-    return processed 
+    return processed
+
+def calculateHappiness(peopleArrangement, connections):
+    result = 0
+    n = len(peopleArrangement)
+
+    for i in range(0,n):
+        personLeft = peopleArrangement[(i-1)%n]
+        personCenter = peopleArrangement[i]
+        personRight = peopleArrangement[(i+1)%n]
+
+        result += connections[(connections["nodeA"]==personCenter) & (connections["nodeB"]==personLeft)]["weight"].values
+        result += connections[(connections["nodeA"]==personCenter) & (connections["nodeB"]==personRight)]["weight"].values
+
+    return result[0]
+
+def calculateBestArrangement(chosen, all, connections, solutions):
+    personFound = False
+    for person in all:
+        if person not in chosen:
+            calculateBestArrangement(chosen+[person], all, connections, solutions)
+            personFound=True
+
+    if not personFound:
+        solutions.append((chosen, calculateHappiness(chosen, connections)))
+        log(len(solutions))
 
 def solution(inputFile):
-    inputData = getInputData(inputFile)
+    connections = getInputData(inputFile)
 
-    log(inputData)
+    people = connections["nodeA"].unique()
 
-    return None
+    solutions = []
+    calculateBestArrangement([], people, connections, solutions)
+
+    solutions = pd.DataFrame(solutions, columns = ["people", "happiness"]).sort_values(by="happiness", ascending=False)
+
+    return solutions.head(1)
