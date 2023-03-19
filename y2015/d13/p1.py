5033b4e994010d5d061216b1f0dd1d1bb11faf2b
Tim <Tim@192.168.1.15>
2023-03-19 23:14:57 +0200

    Started 2015/13

---
+++ y2015/d13/p1.py
@@ -0,0 +1,26 @@
+import sys, os, math
+import numpy as np
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join('../..')))
+
+from utils import matrixUtils, arrayUtils, numberUtils, geometryUtils
+from utils.inputDataUtils import *
+from utils.stringOpUtils import *
+from utils.terminalUtils import *
+from utils.labelMakerUtils import *
+from utils.solutionRoot import *
+ 
+def getInputData(inputFile):
+    raw = getRawLines(inputFile)
+    
+    processed=[entry for entry in raw]
+
+    return processed 
+
+def solution(inputFile):
+    inputData = getInputData(inputFile)
+
+    log(inputData)
+
+    return None
