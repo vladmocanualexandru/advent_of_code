89c7fde10e27f173f66d433b63b07fb710d60568
Vlad Mocanu z00319SB <vlad.mocanu.alexandru@gmail.com>
2023-12-07 10:44:13 +0200

    Added y2023d06

---
+++ y2023/d06/p1.py
@@ -0,0 +1,46 @@
+import sys, os, math
+import numpy as np
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join('../..')))
+
+from utils import matrixUtils, arrayUtils, numberUtils, geometryUtils
+from utils.inputDataUtils import *
+from utils.stringOpUtils import *
+from utils.terminalUtils import *
+from utils.labelMakerUtils import *
+from utils.solutionRoot import *
+
+EXPECTED_RESULT = 275724
+ 
+def getInputData(inputFile):
+    raw = getTuples_text(inputFile, 'Time:', 'Distance:', ' ')
+
+    times = [int(entry) for entry in raw[0] if entry.isnumeric()]
+    distances = [int(entry) for entry in raw[1] if entry.isnumeric()]
+    
+    return (times, distances) 
+
+def solution(inputFile):
+    result = 1
+    
+    (times, distances) = getInputData(inputFile)
+
+    for raceId in range(len(times)):
+        time = times[raceId]
+        distance = distances[raceId]
+
+        attemptCounter=0
+        for attempt in range(int(time/2), 1, -1):
+            if attempt*(time-attempt)<=distance:
+                break
+            attemptCounter+=1
+
+        attemptCounter*=2
+        
+        if (time%2 == 0):
+            attemptCounter-=1
+        
+        result*=attemptCounter
+
+    return (result, EXPECTED_RESULT)
