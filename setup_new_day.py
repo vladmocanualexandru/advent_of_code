e841d352d8a51c879518f3b9e640666d0380aed8
Vlad Mocanu z00319SB <vlad.mocanu.alexandru@gmail.com>
2023-12-11 10:58:44 +0200

    Created template for individual day launch; cleanup

--- setup_new_day.py
+++ setup_new_day.py
@@ -1,10 +1,18 @@
 import os, shutil, fileinput
+yearLabel = yearFolder = 'y'+input("Year:")
+dayLabel = 'd'+"{:02d}".format(int(input("Day:")))
 
-yearFolder = 'y'+input("Year:")
-dayFolder = path = os.path.join(yearFolder, 'd'+"{:02d}".format(int(input("Day:"))))
+dayFolder = path = os.path.join(yearFolder, dayLabel)
 
 os.mkdir(dayFolder)
 
-shutil.copyfile(os.path.join('templates', 'input_template.txt'), os.path.join(dayFolder, 'input.txt'))
-shutil.copyfile(os.path.join('templates', 'p1_template.py'), os.path.join(dayFolder, 'p1.py'))
-shutil.copyfile(os.path.join('templates', 'p2_template.py'), os.path.join(dayFolder, 'p2.py'))
+with open(os.path.join(dayFolder, 'input.txt'), 'w') as tf:
+    pass
+
+shutil.copyfile(os.path.join('templates', 'p.py.template'), os.path.join(dayFolder, 'p1.py'))
+shutil.copyfile(os.path.join('templates', 'p.py.template'), os.path.join(dayFolder, 'p2.py'))
+
+launchFile = open(os.path.join(dayFolder, 'launch.py'), 'wt', encoding='utf-8')
+
+for line in open(os.path.join('templates', 'launch.py.template'), 'r').readlines():
+    launchFile.write(line.replace("{yearLabel}", yearLabel).replace("{dayLabel}", dayLabel))
